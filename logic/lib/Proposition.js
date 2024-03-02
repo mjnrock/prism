602e3351c298d3b3630e3d52357ffe78245457e3
LesPantsFancy <mjnrock@umich.edu>
2024-03-02 17:49:40 -0500

    init

--- logic/lib/Proposition.js
+++ logic/lib/Proposition.js
@@ -40,6 +40,29 @@ export const IF = (a, b) => async () => await OR(NOT(a), b)();
 
 export const IFF = (a, b) => async () => await NOT(XOR(a, b))();
 
+/**
+ * Evaluates a logic circuit. The circuit can be a function that returns a boolean value, an array that represents a logic circuit, or a raw boolean value.
+ * Use if you need to evaluate multiple circuits at once, or if you need to process the array syntax.
+ */
+export const evaluate = async (circuit) => {
+	if(typeof circuit === "function") {
+		return await circuit();
+	} else if(Array.isArray(circuit)) {
+		const operator = circuit[ 0 ];
+		const operands = circuit.slice(1);
+
+		const evaluatedOperands = await Promise.all(operands.map(async operand => await evaluate(operand)));
+
+		if(typeof operator === "function") {
+			return evaluate(await operator(...evaluatedOperands));
+		} else {
+			throw new Error("Invalid operator in logic circuit.");
+		}
+	} else {
+		return circuit;
+	}
+};
+
 export const Proposition = {
 	OR,
 	AND,
@@ -50,13 +73,18 @@ export const Proposition = {
 	XNOR,
 	IF,
 	IFF,
+	evaluate,
 };
 
 export default Proposition;
 
 
-
-// Example usage
+/**
+ * ============================================
+ * 		Example usage
+ * ============================================
+ */
+// // Syntactical variant that uses function syntax
 // const logicCircuit = AND(
 // 	AND(true, NOT(false)),
 // 	NOR(false, false),
@@ -66,4 +94,19 @@ export default Proposition;
 // 	IF(false, true),
 // );
 
-// logicCircuit().then(result => console.log(result));
+// logicCircuit().then(result => console.log(111, result));
+
+// // Syntactical variant that uses array syntax
+// const logicCircuit2 = [
+// 	AND,
+// 	[ AND, true, [ NOT, false ] ],
+// 	[ NOR, false, false ],
+// 	[ XOR, true, false ],
+// 	true,
+// 	() => fetch("https://jsonplaceholder.typicode.com/todos/1").then(response => !response.ok).then(result => !result),
+// 	[ IF, false, true ],
+// ];
+
+// evaluate(logicCircuit).then(result => console.log(222, result));
+// evaluate(logicCircuit2).then(result => console.log(333, result));
+// evaluate([ OR, logicCircuit, logicCircuit2 ]).then(result => console.log(444, result));
